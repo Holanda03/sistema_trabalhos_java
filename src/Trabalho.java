795c779e15f0576f151b901dbbd0e33c5e0e3995
Holanda03 <carlosabraao@hotmail.com.br>
2023-03-06 16:43:20 -0300

    adicionando métodos a classe Trabalho

--- src/Trabalho.java
+++ src/Trabalho.java
@@ -40,4 +40,26 @@ public class Trabalho {
         this.area = area;
         this.participantes = new ArrayList<>();
     }
+
+    public void inserirParticipante(Aluno participante){
+        if (participantes.size() > 2){
+            throw new RuntimeException("O trabalho " + this.titulo + " já possui o número máximo de participantes.");
+        }
+        if (participantes.contains(participante)){
+            throw new RuntimeException("O trabalho " + this.titulo + " já contêm o aluno(a) " + participante.getNome() + ".");
+        } else {
+            participantes.add(participante);
+        }
+    }
+
+    public void removerParticipante(Aluno participante){
+        if (participantes.size() == 0){
+            throw new RuntimeException("O trabalho " + this.titulo + " não possui participantes.");
+        }
+        if (participantes.contains(participante)){
+            participantes.remove(participante);
+        } else {
+            throw new RuntimeException("O trabalho " + this.titulo + " não contêm o aluno(a) " + participante.getNome() + ".");
+        }
+    }
 }
